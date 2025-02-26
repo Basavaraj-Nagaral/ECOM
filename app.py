970e4547fb3b75a3e0335fe5c338c45f17a85bec
Basavaraj Nagaral <basavaraj.nagaral99@gmail.com>
2025-02-26 15:30:51 +0530

    latest changes to host 0.0.0.0

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
+import os
 
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,4 +19,4 @@ def api():
     return jsonify({"message": "Hello, this is the API endpoint!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
